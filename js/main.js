fc047e596f764fb2a6e58f470ea7ac6f6307eb82
o.marchenko <o.marchenko@nekwall.com>
2020-07-15 19:39:42 +0300

    mobile version is added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
-  console.log('WORKING');
+  function copyToClipboard() {
+    const str = document.getElementById('item-to-copy').innerText;
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
+
+  const btnCl = document.querySelector('#btnCl');
+  btnCl.addEventListener('click', () => {
+    copyToClipboard();
+    alert('Witcher has been copied to clipboard');
+  });
 
 });
